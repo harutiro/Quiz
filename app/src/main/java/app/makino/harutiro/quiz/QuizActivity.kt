49aef0c37a0469ee5594e266f0841bf6a1173e06
harutiro <harutiro2027@outlook.jp>
2021-02-27 17:44:13 +0900

    add:　初期画面作成

--- app/src/main/java/app/makino/harutiro/quiz/QuizActivity.kt
+++ app/src/main/java/app/makino/harutiro/quiz/QuizActivity.kt
@@ -3,12 +3,9 @@ package app.makino.harutiro.quiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        
-
+        setContentView(R.layout.activity_quiz)
     }
 }
