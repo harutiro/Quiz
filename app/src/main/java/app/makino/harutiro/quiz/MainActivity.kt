6c714b8943d9688008c13f627d49d50ba6461b76
harutiro <harutiro2027@outlook.jp>
2021-02-27 17:49:12 +0900

    add: Quiz画面へのIntent　完了

--- app/src/main/java/app/makino/harutiro/quiz/MainActivity.kt
+++ app/src/main/java/app/makino/harutiro/quiz/MainActivity.kt
@@ -1,12 +1,22 @@
 package app.makino.harutiro.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val quizIntent: Intent = Intent(this,QuizActivity::class.java)
+
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+
+        }
+
+
 
         
 
