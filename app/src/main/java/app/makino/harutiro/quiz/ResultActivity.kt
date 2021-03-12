fd71ae1e3e4d098a26b43d95f748883c8edb92de
harutiro <harutiro2027@outlook.jp>
2021-03-13 02:00:33 +0900

    製作途中 ３７P

---
+++ app/src/main/java/app/makino/harutiro/quiz/ResultActivity.kt
@@ -0,0 +1,11 @@
+package app.makino.harutiro.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
